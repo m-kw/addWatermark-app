36ea65cdb1944b6945133cda3ae4a78aa42493b5
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-06 12:00:11 +0100

    Add try catch to functions adding watermarks

--- app.js
+++ app.js
@@ -3,36 +3,45 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  const textData = {
-    text: text,
-    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-  };
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    const textData = {
+      text: text,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    };
 
-  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
-  image.quality(100).write(outputFile);
+    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+    image.quality(100).write(outputFile);
 
-  console.log('Adding watermark finished with success!');
-  startApp();
+    console.log('Adding watermark finished with success!');
+    startApp();
+  } catch(error) {
+    console.log(error);
+  }
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
-  const image = await Jimp.read(inputFile);
-  const watermark = await Jimp.read(watermarkFile);
-  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  try {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-  image.composite(watermark, x, y, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 0.5,
-  });
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.5,
+    });
 
-  image.quality(100).write(outputFile);
+    image.quality(100).write(outputFile);
 
-  console.log('Adding watermark finished with success!');
-  startApp();
+    console.log('Adding watermark finished with success!');
+    startApp();
+  }
+  catch(error) {
+    console.log(error);
+  }
 };
 
 const prepareOutputFilename = fileName => {
